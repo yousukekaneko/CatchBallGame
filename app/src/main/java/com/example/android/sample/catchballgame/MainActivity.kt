91dc6b0d9420dfe6baad3e214ac206cdc99a74fb
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2020-07-10 23:44:19 +0900

    画像を画面外に退避

--- app/src/main/java/com/example/android/sample/catchballgame/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/catchballgame/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.android.sample.catchballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        point1.setX(-80.0f)
+        point1.setY(-80.0f)
+        point2.setX(-80.0f)
+        point2.setY(-80.0f)
+        black.setX(-80.0f)
+        black.setY(-80.0f)
+
+        startLabel.visibility = View.INVISIBLE
     }
 }
