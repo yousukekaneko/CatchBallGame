81d3a652e1ec3a40b87b718647d47451fd0b619c
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2020-07-13 23:48:57 +0900

    handlerとtimerの実装

--- app/src/main/java/com/example/android/sample/catchballgame/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/catchballgame/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.android.sample.catchballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    var boyX : Float = 0.0f
 
+    //Handler Timer
+    var handler = Handler()
+    var timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,18 @@ class MainActivity : AppCompatActivity() {
         black.setY(-80.0f)
 
         startLabel.visibility = View.INVISIBLE
+        boyX = 500.0f
+
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            boyX -= 20
+        }
+        lion.setY(boyX)
+
+        return true
     }
 }
